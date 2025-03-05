0ddfe9f5901c6d829328fe80daa5f320a2dee103
Mr Linh <mrlinh@MacBook-Pro-cua-Mr.local>
2025-03-06 01:31:13 +0700

    update

--- main.py
+++ main.py
@@ -21,17 +21,35 @@ while running:
     if index == 0:
         dmg = player.generate_damage()
         enemy.take_damage(dmg)
-        print('You attack for', dmg, 'points of damage. Enemy HP:', enemy.get_hp())
+        print('You attack for', dmg, 'points of damage')
     elif index == 1:
         player.choose_magic()
         magic_choice = int(input('Choose magic')) - 1
         magic_dmg = player.generate_spell_damage(magic_choice)
+        spell = player.get_spell_name(magic_choice)
+        cost = player.get_spell_mp_cost(magic_choice)
+        
+        current_mp = player.get_mp()
+        
+        if cost > current_mp:
+            print(bcolors.FAIL + '\nNot enough MP\n' + bcolors.ENDC)
+            continue
+            
+        player.reduce_mp(cost)
         enemy.take_damage(magic_dmg)
+        
+        print(bcolors.OKBLUE + '\n' + spell + ' deals', str(magic_dmg), 'points of damage' + bcolors.ENDC)
     
     enemy_choice = 1
     enemy_dmg = enemy.generate_damage()
     player.take_damage(enemy_dmg)
-    print('Enemy attack for', enemy_dmg, 'points of damage. your HP:', player.get_hp())
+    print('Enemy attack for', enemy_dmg)
+    
+    print('----------------------------- ')
+    
+    print('Enemy HP', bcolors.FAIL + str(enemy.get_hp()) + '/' + str(enemy.get_max_hp()) + bcolors.ENDC + '\n')
+    print('Your HP:', bcolors.OKGREEN + str(player.get_hp()) + '/' + str(player.get_max_hp()) + bcolors.ENDC)
+    print('Your MP: ', bcolors.OKBLUE + str(player.get_mp()) + '/' + str(player.get_max_mp()) + bcolors.ENDC + '\n')
     
     if enemy.get_hp() == 0:
         print(bcolors.OKGREEN + "You win!" + bcolors.ENDC)
